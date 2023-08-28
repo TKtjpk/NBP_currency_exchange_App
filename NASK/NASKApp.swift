b0cba95fb294f0d116372046e83e4b81a2fb1348
Tomasz Kubicki <80661109+TKtjpk@users.noreply.github.com>
2023-08-28 18:55:24 +0200

    Initial Commit

---
+++ NASK/NASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASKApp.swift
+//  NASK
+//
+//  Created by Tomasz Kubicki on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
